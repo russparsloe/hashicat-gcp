06899326f36c212ae799e06588e339b8825cad75
Russ Parsloe <russparsloe@users.noreply.github.com>
2022-12-02 09:15:46 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,13 @@
+module "network" {
+  source  = "app.terraform.io/rusty-pro/network/google"
+  version = "3.4.0"
+  network_name = "russp"
+  project_id = var.project
+  subnets = [
+  {
+    subnet_name   = "russp-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
